82b1c17de0c2239394ce7d926c395ef4990fb158
Ricardo Temple <122487111+RickMaverick@users.noreply.github.com>
2024-04-25 22:03:14 -0300

    adiciona teste com mock, consulta estado de Pernambuco

--- src/test/java/org/estudos/br/ConsultaIBGETest.java
+++ src/test/java/org/estudos/br/ConsultaIBGETest.java
@@ -1,17 +1,38 @@
 package org.estudos.br;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.RETURNS_MOCKS;
+import static org.mockito.Mockito.when;
 
 public class ConsultaIBGETest {
+    @Mock
+    private HttpURLConnection connectionMock;
+
     private static final String ESTADOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
 
+    private static final String JSON_MOCK_ESTADOS_PE = "{\"id\":26,\"sigla\":\"PE\",\"nome\":\"Pernambuco\",\"regiao\":{\"id\":2,\"sigla\":\"NE\",\"nome\":\"Nordeste\"}}";
+
     private static final String DISTRITOS_API_URL = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos/";
+
+    @BeforeEach
+    public void setup() throws IOException{
+        MockitoAnnotations.openMocks(this);
+
+        InputStream inputStreamStateAC = new ByteArrayInputStream(JSON_MOCK_ESTADOS_PE.getBytes());
+        when(connectionMock.getInputStream()).thenReturn(inputStreamStateAC);
+    }
 
     @Test
     @DisplayName("Teste para consulta única de um estado")
@@ -70,4 +91,13 @@ public class ConsultaIBGETest {
         assertEquals(200, statusCode, "O status code da resposta da API deve ser 200 (OK)");
     }
 
+    @Test
+    @DisplayName("Teste Mockado de consulta de Estado: ")
+    public void testMockConsultarEstadoPernambuco() throws IOException {
+        String siglaEstado = "PE";
+
+        String response = ConsultaIBGE.consultarEstado(siglaEstado);
+
+        assertEquals(JSON_MOCK_ESTADOS_PE, response, "Há divergencia no body da response");
+    }
 }
